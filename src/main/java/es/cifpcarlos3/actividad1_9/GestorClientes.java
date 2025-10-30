d4cd3e8af0e685a5f9bde2869d5b5ab1368a2474
Jorge <11655449@alu.murciaeduca.es>
2025-10-30 19:30:14 +0100

    commit clases

--- src/main/java/es/cifpcarlos3/actividad1_9/GestorClientes.java
+++ src/main/java/es/cifpcarlos3/actividad1_9/GestorClientes.java
@@ -1,4 +1,24 @@
 package es.cifpcarlos3.actividad1_9;
 
+import es.cifpcarlos3.actividad1_9.vo.ListaClientes;
+import tools.jackson.databind.DeserializationFeature;
+import tools.jackson.dataformat.xml.XmlMapper;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
 public class GestorClientes {
+    public static void main(String[] args) {
+        Path base = Path.of("src","main","java","es","cifpcarlos3","actividad1_9");
+        Path XML = base.resolve("clientes.xml");
+
+        var xmlMapper = XmlMapper.builder().enable(DeserializationFeature.UNWRAP_ROOT_VALUE).build();
+        try (var br = Files.newBufferedReader(XML, StandardCharsets.UTF_8)) {
+            ListaClientes datos = xmlMapper.readValue(br, ListaClientes.class);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/es/cifpcarlos3/actividad1_9/vo/ListaClientes.java
@@ -0,0 +1,4 @@
+package es.cifpcarlos3.actividad1_9.vo;
+
+public class ListaClientes {
+}

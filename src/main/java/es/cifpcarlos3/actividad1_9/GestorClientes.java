29c309ba90276d6db743d370f2d5b0319036128c
Jorge <11655449@alu.murciaeduca.es>
2025-11-02 18:23:25 +0100

    commit clases 4

--- src/main/java/es/cifpcarlos3/actividad1_9/GestorClientes.java
+++ src/main/java/es/cifpcarlos3/actividad1_9/GestorClientes.java
@@ -1,5 +1,6 @@
 package es.cifpcarlos3.actividad1_9;
 
+import es.cifpcarlos3.actividad1_9.vo.Cliente;
 import es.cifpcarlos3.actividad1_9.vo.ListaClientes;
 import tools.jackson.databind.DeserializationFeature;
 import tools.jackson.dataformat.xml.XmlMapper;
@@ -16,6 +17,12 @@ public class GestorClientes {
         var xmlMapper = XmlMapper.builder().enable(DeserializationFeature.UNWRAP_ROOT_VALUE).build();
         try (var br = Files.newBufferedReader(XML, StandardCharsets.UTF_8)) {
             ListaClientes datos = xmlMapper.readValue(br, ListaClientes.class);
+            for (Cliente cliente : datos.clientes) {
+                if (cliente.getId() == 0 || cliente.getNombre() == null) {
+                    System.out.println("Cliente no valido");
+                    System.exit(0);
+                }
+            }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
--- src/main/java/es/cifpcarlos3/actividad1_9/vo/Cliente.java
+++ src/main/java/es/cifpcarlos3/actividad1_9/vo/Cliente.java
@@ -1,10 +1,16 @@
 package es.cifpcarlos3.actividad1_9.vo;
 
-
+import lombok.Data;
+import tools.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import tools.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import java.util.List;
 
+@Data
 public  class Cliente {
-    long id;
-    String nombre;
-    List<Sucursal> sucursales;
+    @JacksonXmlProperty(isAttribute = true)
+    private long id;
+    private String nombre;
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private List<Sucursal> sucursal;
 }

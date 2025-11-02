3f23faf58ac0c71110d357ac971cb53e1bbd6441
Jorge <11655449@alu.murciaeduca.es>
2025-11-02 18:51:55 +0100

    commit clases 5

--- src/main/java/es/cifpcarlos3/actividad1_9/GestorClientes.java
+++ src/main/java/es/cifpcarlos3/actividad1_9/GestorClientes.java
@@ -2,12 +2,14 @@ package es.cifpcarlos3.actividad1_9;
 
 import es.cifpcarlos3.actividad1_9.vo.Cliente;
 import es.cifpcarlos3.actividad1_9.vo.ListaClientes;
+import es.cifpcarlos3.actividad1_9.vo.Sucursal;
 import tools.jackson.databind.DeserializationFeature;
 import tools.jackson.dataformat.xml.XmlMapper;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class GestorClientes {
     public static void main(String[] args) {
@@ -17,14 +19,35 @@ public class GestorClientes {
         var xmlMapper = XmlMapper.builder().enable(DeserializationFeature.UNWRAP_ROOT_VALUE).build();
         try (var br = Files.newBufferedReader(XML, StandardCharsets.UTF_8)) {
             ListaClientes datos = xmlMapper.readValue(br, ListaClientes.class);
-            for (Cliente cliente : datos.clientes) {
-                if (cliente.getId() == 0 || cliente.getNombre() == null) {
-                    System.out.println("Cliente no valido");
-                    System.exit(0);
-                }
-            }
+            validarClientes(datos);
+            mostrarClientes(datos);
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+    public static void validarClientes(ListaClientes datos){
+        for (Cliente cliente : datos.clientes) {
+            if (cliente.getId() == 0 || cliente.getNombre() == null) {
+                System.out.println("Cliente no valido");
+                System.exit(0);
+            }
+            List<Sucursal> sucursales = cliente.getSucursal();
+            if (sucursales == null || sucursales.isEmpty()) {
+                System.err.println("Error: Cliente sin sucursales.");
+                System.exit(0);
+            }
+            for (Sucursal sucurse : sucursales) {
+                if (sucurse.getCalle() == null || sucurse.getCp() == null ||
+                    sucurse.getCiudad() == null || sucurse.getProvincia() == null) {
+                    System.err.println("Cliente no valido.");
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    public static void mostrarClientes(ListaClientes datos){
+
     }
 }
